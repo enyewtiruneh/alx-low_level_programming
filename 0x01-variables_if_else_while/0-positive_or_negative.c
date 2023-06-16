ebe918268260c28610324c51228b310a5356eea6
enyewtiruneh <enyewtiruneh@gmail.com>
2023-06-16 13:19:14 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,15 @@
  * Return: Always (Success)
  *
  */
+
 int main(void)
+
 {
+
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if(n > 0)
 {
 printf("%d is positive\n", n);
@@ -24,4 +28,5 @@ else
 printf("%d is negative\n", n);
 }
 return (0);
+
 }
