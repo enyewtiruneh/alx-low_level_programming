446ca90d7a3ce3dd12f63656dcb822e008e2c0bb
enyewtiruneh <enyewtiruneh@gmail.com>
2023-06-18 03:14:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main-Entry post
+* Main - Entry point
 *
-*Return Always 0 (Success)
+* Return Always 0 (Success)
 */
 int main(void)
 {
-	puts("\"Progtammming is like building a multilingual puzzle");
+	puts("Progtammming is like building a multilingual puzzle");
 	return (0);
 }
